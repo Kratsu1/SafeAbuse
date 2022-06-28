49a01265ff3b93c56e4de3d91ef4dcaadbf86270
Kratsu <david.v.b@gmx.de>
2022-06-28 14:20:48 +0200

    Bessere Timer

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,27 +29,36 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SafeAbuse));
             this.txtTimeStand = new System.Windows.Forms.TextBox();
             this.lblStand = new System.Windows.Forms.Label();
             this.txtTimeSit = new System.Windows.Forms.TextBox();
             this.lblSit = new System.Windows.Forms.Label();
             this.btnStartTimer = new System.Windows.Forms.Button();
             this.btnStopTimer = new System.Windows.Forms.Button();
-            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.timerStand = new System.Windows.Forms.Timer(this.components);
+            this.lblinfo = new System.Windows.Forms.Label();
+            this.lblSecondCounter = new System.Windows.Forms.Label();
+            this.timerSeconds = new System.Windows.Forms.Timer(this.components);
+            this.timerSit = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // txtTimeStand
             // 
-            this.txtTimeStand.Location = new System.Drawing.Point(141, 27);
+            this.txtTimeStand.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(66)))), ((int)(((byte)(80)))));
+            this.txtTimeStand.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.txtTimeStand.ForeColor = System.Drawing.Color.White;
+            this.txtTimeStand.Location = new System.Drawing.Point(156, 33);
             this.txtTimeStand.Name = "txtTimeStand";
-            this.txtTimeStand.Size = new System.Drawing.Size(172, 20);
+            this.txtTimeStand.Size = new System.Drawing.Size(157, 23);
             this.txtTimeStand.TabIndex = 0;
             // 
             // lblStand
             // 
             this.lblStand.AutoSize = true;
             this.lblStand.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblStand.Location = new System.Drawing.Point(12, 20);
+            this.lblStand.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(88)))), ((int)(((byte)(78)))));
+            this.lblStand.Location = new System.Drawing.Point(12, 27);
             this.lblStand.Name = "lblStand";
             this.lblStand.Size = new System.Drawing.Size(113, 26);
             this.lblStand.TabIndex = 1;
@@ -57,17 +66,21 @@
             // 
             // txtTimeSit
             // 
-            this.txtTimeSit.Location = new System.Drawing.Point(141, 89);
+            this.txtTimeSit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(66)))), ((int)(((byte)(80)))));
+            this.txtTimeSit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtTimeSit.ForeColor = System.Drawing.Color.White;
+            this.txtTimeSit.Location = new System.Drawing.Point(156, 89);
             this.txtTimeSit.Name = "txtTimeSit";
-            this.txtTimeSit.Size = new System.Drawing.Size(172, 20);
+            this.txtTimeSit.Size = new System.Drawing.Size(157, 20);
             this.txtTimeSit.TabIndex = 2;
             // 
             // lblSit
             // 
             this.lblSit.AutoSize = true;
             this.lblSit.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.lblSit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(88)))), ((int)(((byte)(78)))));
             this.lblSit.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.lblSit.Location = new System.Drawing.Point(12, 70);
+            this.lblSit.Location = new System.Drawing.Point(12, 82);
             this.lblSit.Name = "lblSit";
             this.lblSit.Size = new System.Drawing.Size(82, 26);
             this.lblSit.TabIndex = 3;
@@ -77,9 +90,9 @@
             // 
             this.btnStartTimer.BackColor = System.Drawing.Color.Lime;
             this.btnStartTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnStartTimer.Location = new System.Drawing.Point(141, 134);
+            this.btnStartTimer.Location = new System.Drawing.Point(156, 134);
             this.btnStartTimer.Name = "btnStartTimer";
-            this.btnStartTimer.Size = new System.Drawing.Size(82, 67);
+            this.btnStartTimer.Size = new System.Drawing.Size(67, 67);
             this.btnStartTimer.TabIndex = 4;
             this.btnStartTimer.Text = "Start";
             this.btnStartTimer.UseVisualStyleBackColor = false;
@@ -87,7 +100,7 @@
             // 
             // btnStopTimer
             // 
-            this.btnStopTimer.BackColor = System.Drawing.Color.Red;
+            this.btnStopTimer.BackColor = System.Drawing.Color.DarkOrange;
             this.btnStopTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnStopTimer.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.btnStopTimer.Location = new System.Drawing.Point(238, 134);
@@ -99,16 +112,50 @@
             this.btnStopTimer.DoubleClick += new System.EventHandler(this.btnResetTimer_Click);
             this.btnStopTimer.Click += new System.EventHandler(this.btnStopTimer_Click);
             // 
-            // timer
+            // timerStand
             // 
-            this.timer.Interval = 1000;
-            this.timer.Tick += new System.EventHandler(this.Timer_Tick);
+            this.timerStand.Interval = 1000;
+            this.timerStand.Tick += new System.EventHandler(this.TimerStand_Tick);
+            // 
+            // lblinfo
+            // 
+            this.lblinfo.AutoSize = true;
+            this.lblinfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.lblinfo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(20)))), ((int)(((byte)(95)))));
+            this.lblinfo.Location = new System.Drawing.Point(14, 145);
+            this.lblinfo.Name = "lblinfo";
+            this.lblinfo.Size = new System.Drawing.Size(121, 34);
+            this.lblinfo.TabIndex = 7;
+            this.lblinfo.Text = "* Please enter the\r\ntime in minutes.";
+            // 
+            // lblSecondCounter
+            // 
+            this.lblSecondCounter.AutoSize = true;
+            this.lblSecondCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.lblSecondCounter.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(88)))), ((int)(((byte)(78)))));
+            this.lblSecondCounter.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lblSecondCounter.Location = new System.Drawing.Point(153, 9);
+            this.lblSecondCounter.Name = "lblSecondCounter";
+            this.lblSecondCounter.Size = new System.Drawing.Size(0, 17);
+            this.lblSecondCounter.TabIndex = 8;
+            // 
+            // timerSeconds
+            // 
+            this.timerSeconds.Interval = 1000;
+            this.timerSeconds.Tick += new System.EventHandler(this.TimerSeconds_Tick);
+            // 
+            // timerSit
+            // 
+            this.timerSit.Interval = 1000;
+            this.timerSit.Tick += new System.EventHandler(this.TimerSit_Tick);
             // 
             // SafeAbuse
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(354, 237);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(326, 220);
+            this.Controls.Add(this.lblSecondCounter);
+            this.Controls.Add(this.lblinfo);
             this.Controls.Add(this.btnStopTimer);
             this.Controls.Add(this.btnStartTimer);
             this.Controls.Add(this.lblSit);
@@ -116,10 +163,9 @@
             this.Controls.Add(this.lblStand);
             this.Controls.Add(this.txtTimeStand);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "SafeAbuse";
-            this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SafeAbuse";
             this.ResumeLayout(false);
@@ -135,7 +181,11 @@
         private System.Windows.Forms.Label lblSit;
         private System.Windows.Forms.Button btnStartTimer;
         private System.Windows.Forms.Button btnStopTimer;
-        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Timer timerStand;
+        private System.Windows.Forms.Label lblinfo;
+        private System.Windows.Forms.Label lblSecondCounter;
+        private System.Windows.Forms.Timer timerSeconds;
+        private System.Windows.Forms.Timer timerSit;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,7 @@ namespace SafeAbuse
     {
         private int standTime;
         private int sitTime;
+        private int seconds = 60;
         private bool showAlert = true;
 
         public SafeAbuse()
@@ -23,72 +24,79 @@ namespace SafeAbuse
 
         private void btnStopTimer_Click(object sender, EventArgs e)
         {
-            timer.Stop();
+            StopTimers();
         }
 
         private void btnResetTimer_Click(object sender, EventArgs e)
         {
-            timer.Stop();
+            StopTimers();
+            StartTimers(1);
+
+            seconds = 60;
             lblStand.Text = "Stand (00)";
             lblSit.Text = "Sit (00)";
         }
 
+        private void StopTimers()
+        {
+            timerStand.Stop();
+            timerSit.Stop();
+            timerSeconds.Stop();
+        }
+        private void StartTimers(int identifier)
+        {
+            if (identifier == 1)
+                timerStand.Start();
+            else
+                timerSit.Start();
+            timerSeconds.Start();
+        }
+
         private void btnStartTimer_Click(object sender, EventArgs e)
         {
-            timer.Start();
+            lblStand.Text = "Stand (" + txtTimeStand.Text + ")";
+            lblSit.Text = "Sit (" + txtTimeSit.Text + ")";
+            StartTimers(1);
         }
 
-        private void Timer_Tick(object sender, EventArgs e)
+        private void TimerStand_Tick(object sender, EventArgs e)
         {
+            int.TryParse(lblStand.Text.Split('(')[1].Split(')')[0], out standTime);
 
-            int.TryParse(lblStand.Text.Split('(')[1].Split(')')[0], out int lblStandTimer);
-            int.TryParse(lblSit.Text.Split('(')[1].Split(')')[0], out int lblSitTimer);
+            standTime--;
+            lblStand.Text = "Stand (" + standTime + ")";
 
-            if (lblSitTimer == 0 || lblStandTimer != 0)
+            if (standTime == 0)
             {
-                if(showAlert)
-                {
-                    showAlert = false;
-                    MessageBox.Show("Stand-Up");
-                }
-
-                if (lblStandTimer != 0)
-                {
-                    lblStand.Text = "Stand (" + standTime + ")";
-                }
-
-                else if (int.TryParse(txtTimeStand.Text, out standTime))
-                {
-                    lblStand.Text = "Stand (" + standTime + ")";
-                }
-                standTime--;
-
-                if (lblStandTimer == 1)
-                    showAlert = true;
-            }
-
-            if(lblStandTimer == 0)
-            {
-
-                if (showAlert)
-                {
-                    showAlert = false;
-                    MessageBox.Show("Sit-Down");
-                }
-
-                if(lblSitTimer != 0)
-                {
-                    lblSit.Text = "Sit (" + sitTime + ")";
-                }
-                else if (int.TryParse(txtTimeSit.Text, out sitTime))
-                {
-                    lblSit.Text = "Sit (" + sitTime + ")";
-                }
-                sitTime--;
-
-                if (lblSitTimer == 1)
-                    showAlert = true;
+                lblStand.Text = "Stand (" + txtTimeStand.Text + ")";
+                StopTimers();
+                StartTimers(2);
             }
         }
+
+        private void TimerSit_Tick(object sender, EventArgs e)
+        {
+            int.TryParse(lblSit.Text.Split('(')[1].Split(')')[0], out sitTime);
+
+            sitTime--;
+
+            if (sitTime == 0)
+            {
+                lblSit.Text = "Sit (" + txtTimeSit.Text + ")";
+                StopTimers();
+                StartTimers(1);
+            }
+        }
+
+        private void TimerSeconds_Tick(object sender, EventArgs e)
+        {
+            lblSecondCounter.Text = seconds.ToString();
+            if(seconds == 1)
+            {
+                seconds = 60;
+            }
+            seconds--;
+        }
+
     }
 }
